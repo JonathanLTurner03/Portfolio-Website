7d6ddfb5b1e6d47c776ff2c42f89c2685ba67e58
Jonathan Turner <turner@atlantissrv.com]>
2024-03-14 16:25:22 -0400

    Created the about page.

--- components/header-element.js
+++ components/header-element.js
@@ -28,6 +28,9 @@ app.component('header-element', {
                 <div class="collapse navbar-collapse text-end" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto float-right text-right pr-3">
                         <li class="nav-item collapsed-nav-item">
+                            <a class="nav-link primary poppins-regular" :class="{ currentPage: page === 'about' }" href="/about">About</a>
+                        </li>
+                        <li class="nav-item collapsed-nav-item">
                             <a class="nav-link primary poppins-regular" :class="{ currentPage: page === 'projects' }"  href="/projects">Projects</a>
                         </li>
                         <li class="nav-item collapsed-nav-item">
