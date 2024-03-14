f006bb43dbeb13b353391ccd1a21750437f2aae5
Jonathan Turner <turner@atlantissrv.com]>
2024-03-14 10:25:03 -0400

    Fixed button links.

--- src/components/main/hero-element.js
+++ src/components/main/hero-element.js
@@ -30,7 +30,7 @@ app.component('hero-element', {
                     </p>
                     <div class="d-grid gap-2 d-md-flex justify-content-md-start">
                         <button type="button" class="btn btn-primary btn-lg px-4 me-md-2" @click="scrollTo('featuredProjects')">Featured Projects</button>
-                        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Resume</button>
+                        <a href="./resume"><button type="button" class="btn btn-outline-secondary btn-lg px-4">Resume</button></a>
                     </div>
                 </div>
             </div>
