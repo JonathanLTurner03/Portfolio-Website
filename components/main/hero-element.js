e219f807610bcebec5edd860f555b654744c757b
Jonathan Turner <turner@atlantissrv.com]>
2024-03-14 16:01:09 -0400

    Fixed error.

--- components/main/hero-element.js
+++ components/main/hero-element.js
@@ -12,7 +12,7 @@ app.component('hero-element', {
     },template:
          `
          <div class="container col-xxl-8 px-4 py-5 hero-cont">
-            <div class="row flex-lg-row-reverse align-items-center g-5 py-5 hero"">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5 hero">
                 <div class="col-10 col-sm-8 col-lg-6 pr-sm-5 hero-image-container">
                     <picture>
                         <source srcset="/resources/images/hero/Mobile.jpg" media="(max-width: 991px)">
