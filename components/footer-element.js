32680d620e32e5ec3df9a7eaf5b452874d4d43a0
Jonathan Turner <turner@atlantissrv.com>
2024-03-15 23:31:20 -0400

    Changed Footer Website link to Repo.

--- components/footer-element.js
+++ components/footer-element.js
@@ -23,13 +23,16 @@ app.component('footer-element', {
         },
         emailLink() {
             return "mailto:" + this.email;
+        },
+        githubRepoLink() {
+            return "https://github.com/" + this.github + "/" + "/Portfolio-Website";
         }
     },template:
         `
          <div class="container">
             <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
                 <div class="col-md-4 d-flex align-items-baseline">
-                    <a href="#" class="poppins-regular footer mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                    <a :href="githubRepoLink" class="poppins-regular footer mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                         Turner
                     </a>
                     <span class="mb-3 mb-md-0 text-muted poppins-regular">Licensed under
