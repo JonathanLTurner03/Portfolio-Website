46380283b1a4cfcde45cf1172d47213037ea2023
Jonathan Turner <turner@atlantissrv.com>
2024-07-27 23:43:21 -0400

    I didn't mess it all up lol

--- components/about/photography.js
+++ components/about/photography.js
@@ -5,13 +5,27 @@ app.component('photography', {
         return {
 
         }
-    },template:
+    }, mounted() {
+        document.addEventListener('visibilitychange', function () {
+            const scrollingContainer = document.querySelector('.scrolling-container');
+            if (document.hidden) {
+                scrollingContainer.style.animationPlayState = 'paused';
+            } else {
+                scrollingContainer.style.animationPlayState = 'running';
+            }
+        });
+    }, template:
         `
          <div class="container col-xxl-8 px-4 py-xxl-4 py-lg-3 about-me-cont">
-            <div class="row flex-lg-row align-items-center g-5 py-5 about border-top">
+            <div class="row flex-lg-row-reverse align-items-center g-5 py-5 about border-top">
                 <div class="col-10 col-sm-8 col-lg-6 pr-sm-5 photography-imgs">
+                <div class="scrolling-container">
                     <picture>
                         <img src="/resources/images/about-me/stars.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/stars%20w%20moon.jpg" class="d-block mx-md-auto img-fluid p-img"
                         loading="lazy">
                     </picture>
                     <picture>
@@ -19,13 +33,59 @@ app.component('photography', {
                         loading="lazy">
                     </picture>
                     <picture>
+                        <img src="/resources/images/about-me/birbtakeoff.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/birb%20flying.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
                         <img src="/resources/images/about-me/still.jpg" class="d-block mx-md-auto img-fluid p-img"
                         loading="lazy">
                     </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/sun-lens-flare.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/stars 2.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <!-- Repeating for animation -->
+                    <picture>
+                        <img src="/resources/images/about-me/stars.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/stars%20w%20moon.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/birb.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/birbtakeoff.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/still.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/sun-lens-flare.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/stars 2.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                </div>
                 </div>
                 <div class="col-lg-6">
                     <h1 class="display-7 lh-1 mb-3 poppins-extrabold" style="font-weight: bold">
-                        // Drones
+                        // Photography
                     </h1>
                     <p class="lead poppins-regular">
                         Beyond my academic pursuits, a long standing hobby and interest of mine is 
