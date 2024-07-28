8ff7ee0d9dc904b9ee14a4341dac355b84539498
Jonathan Turner <turner@atlantissrv.com>
2024-07-28 11:17:34 -0400

    Removed scroll notice on mobile and fixed carousel.

--- components/about/photography.js
+++ components/about/photography.js
@@ -18,14 +18,14 @@ app.component('photography', {
         `
          <div class="container col-xxl-8 px-4 py-xxl-4 py-lg-3 about-me-cont">
             <div class="row flex-lg-row-reverse align-items-center g-5 py-5 about border-top">
-                <div class="col-10 col-sm-8 col-lg-6 pr-sm-5 photography-imgs">
+                <div class="col-sm-10 col-sm-8 col-lg-6 pr-sm-5 photography-imgs">
                 <div class="scrolling-container">
                     <picture>
                         <img src="/resources/images/about-me/photography/stars.jpg" class="d-block mx-md-auto img-fluid p-img"
                         loading="lazy">
                     </picture>
                     <picture>
-                        <img src="/resources/images/about-me/stars%20w%20moon.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        <img src="/resources/images/about-me/photography/stars%20w%20moon.jpg" class="d-block mx-md-auto img-fluid p-img"
                         loading="lazy">
                     </picture>
                     <picture>
@@ -37,7 +37,7 @@ app.component('photography', {
                         loading="lazy">
                     </picture>
                     <picture>
-                        <img src="/resources/images/about-me/birb%20flying.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        <img src="/resources/images/about-me/photography/birb%20flying.jpg" class="d-block mx-md-auto img-fluid p-img"
                         loading="lazy">
                     </picture>
                     <picture>
@@ -58,7 +58,7 @@ app.component('photography', {
                         loading="lazy">
                     </picture>
                     <picture>
-                        <img src="/resources/images/about-me/stars%20w%20moon.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        <img src="/resources/images/about-me/photography/stars%20w%20moon.jpg" class="d-block mx-md-auto img-fluid p-img"
                         loading="lazy">
                     </picture>
                     <picture>
@@ -67,6 +67,10 @@ app.component('photography', {
                     </picture>
                     <picture>
                         <img src="/resources/images/about-me/photography/birbtakeoff.jpg" class="d-block mx-md-auto img-fluid p-img"
+                        loading="lazy">
+                    </picture>
+                    <picture>
+                        <img src="/resources/images/about-me/photography/birb%20flying.jpg" class="d-block mx-md-auto img-fluid p-img"
                         loading="lazy">
                     </picture>
                     <picture>
@@ -85,23 +89,15 @@ app.component('photography', {
                 </div>
                 <div class="col-lg-6">
                     <h1 class="display-7 lh-1 mb-3 poppins-extrabold" style="font-weight: bold">
-                        // Photography
+                        // Photography & Nature
                     </h1>
                     <p class="lead poppins-regular">
-                        Beyond my academic pursuits, a long standing hobby and interest of mine is 
-                        drones, particularly for the unique perspectives they offer. This fascination has grown over 
-                        the years, as drones provide a way to explore and capture views that are otherwise inaccessible. 
-                        Whether it's an aerial view of a landscape, a dive down a mountain, or a bird's eye view of a 
-                        city, I enjoy discovering new angles and vistas. 
-                    </p>
-                    <br>
-                    <p class="lead poppins-regular">
-                        Drones have not only been a source of entertainment and exploration, but have also led into
-                        professional interests in the use of drones in various industries. I am particularly interested
-                        in the use of drones in data collection, such as surveying, mapping, and monitoring various 
-                        environments. It has created a strong interest to applying my skills and knowledge in 
-                        Machine/Deep Learning to develop ways of processing and categorizing the data that could be 
-                        easily collected by drones in difficult to navigate environments. 
+                        In addition to my interest in drones, I am an avid hiker. Photography is a significant part of 
+                        my hikes, as I love capturing the essence of the places I visit. From breathtaking landscapes 
+                        to hidden gems in nature, I enjoy documenting my journeys through my camera lens. Long exposure 
+                        photography allows me to capture the stunning night sky and mesmerizing sunsets, providing a 
+                        unique way to remember the places I've been. This hobby has taken me to various locations, both 
+                        familiar and remote, and has increased my appreciation for the world around us.
                     </p>
                 </div>
             </div>
